f354de94e50da9cd46867698a0725472fa49750e
lantianwei <xiaolanlub@outlook.com>
2023-11-13 13:36:54 +1100

    feat: pwa copy writing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,20 +8,22 @@ export default class _Document extends Document {
     return (
       <Html>
         <Head>
-          <meta name="application-name" content="PWA App" />
+          <meta name="application-name" content="Smart Book" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
             content="default"
           />
-          <meta name="apple-mobile-web-app-title" content="PWA App" />
-          <meta name="description" content="Best PWA App in the world" />
+          <meta name="apple-mobile-web-app-title" content="Smart Book" />
+          <meta name="description" content="Manege event booking painlessly" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
 
           <meta name="msapplication-TileColor" content="#2B5797" />
           <meta name="msapplication-tap-highlight" content="no" />
-          <meta name="theme-color" content="#000000" />
+          <meta name="theme-color" content="#f8f9fa" />
+
+          <link rel="manifest" href="/manifest.json" />
 
           <link rel="apple-touch-icon" href="/icons/icons/icon-152x152.png" />
           <link
