dc1e997e59f84b0732ba51f240faeee80bc5c43d
lantianwei <xiaolanlub@outlook.com>
2023-11-13 13:18:27 +1100

    feat: add manifests

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,29 @@ export default class _Document extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta name="application-name" content="PWA App" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="PWA App" />
+          <meta name="description" content="Best PWA App in the world" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
+
+          <link rel="apple-touch-icon" href="/icons/icons/icon-152x152.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/icons/icon-152x152.png"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
